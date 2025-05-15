e719918b81176f0c1841190a9a21ff0b54be7460
BramGeboers <bramgeboers2@gmail.com>
2025-05-15 16:38:05 +0200

    fix: update security configuration to permit access to competitions and scraping endpoints

--- src/main/java/ucll/be/procyclingscraper/controller/CompetitionController.java
+++ src/main/java/ucll/be/procyclingscraper/controller/CompetitionController.java
@@ -3,17 +3,16 @@ package ucll.be.procyclingscraper.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import ucll.be.procyclingscraper.model.Competition;
 import ucll.be.procyclingscraper.service.CompetitionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/competitions")
 public class CompetitionController {
