7b566d60501ab0a90834a7e65ca65f1a6556651d
SanderDebroyer-UCLL <sander.debroyer@student.ucll.be>
2025-05-15 14:48:47 +0200

    temp

---
+++ src/main/java/ucll/be/procyclingscraper/controller/ResultController.java
@@ -0,0 +1,26 @@
+package ucll.be.procyclingscraper.controller;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ucll.be.procyclingscraper.service.ResultService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/results")
+public class ResultController {
+    
+    @Autowired
+    private ResultService resultService;
+    
+    @GetMapping("/scrap")
+    public Result scrapeResults() {
+        return resultService.scrapeResult();
+    }
+    
+}
---
+++ src/main/java/ucll/be/procyclingscraper/model/TimeResult.java
@@ -0,0 +1,18 @@
+package ucll.be.procyclingscraper.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TimeResult extends Result{
+    private LocalDateTime time;
+}
