1d2d226f3493dde2eb2dee56cc5d74d52b71a3ae
SanderDebroyer-UCLL <sander.debroyer@student.ucll.be>
2025-05-20 13:13:22 +0200

    in comment

--- src/main/java/ucll/be/procyclingscraper/cronjobs/ScraperScheduler.java
+++ src/main/java/ucll/be/procyclingscraper/cronjobs/ScraperScheduler.java
@@ -37,7 +37,7 @@ public class ScraperScheduler {
 
         stageService.scrapeStages();
 
-        resultService.scrapeTimeResult();
+        // resultService.scrapeTimeResult();
         
     }
 }
