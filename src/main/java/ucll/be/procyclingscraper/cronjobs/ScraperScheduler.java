7516711bbd8616920530dfcb55be7a46c69b42d2
Niels Wellens <niels.wellens@student.ucll.be>
2025-05-20 09:26:23 +0200

    cronjobs af

--- src/main/java/ucll/be/procyclingscraper/cronjobs/ScraperScheduler.java
+++ src/main/java/ucll/be/procyclingscraper/cronjobs/ScraperScheduler.java
@@ -4,12 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import ucll.be.procyclingscraper.service.CyclistService;
 import ucll.be.procyclingscraper.service.RaceService;
+import ucll.be.procyclingscraper.service.ResultService;
+import ucll.be.procyclingscraper.service.StageService;
+import ucll.be.procyclingscraper.service.TeamService;
 
 @Component
 public class ScraperScheduler {
     @Autowired
     private RaceService raceService;
     
+    @Autowired
+    private TeamService teamService;
+
+    @Autowired 
+    private CyclistService cyclistService;
+
+    @Autowired 
+    private StageService stageService;
     
+    @Autowired
+    private ResultService resultService;
+
+    @Scheduled(cron = "0 * 1 * * *")
+    public void runRaceScraper() {
+        teamService.scrape();
+
+        cyclistService.scrapeCyclists();
+
+        raceService.scrapeRaces();
+
+        stageService.scrapeStages();
+
+        resultService.scrapeResult();
+        
+    }
 }
