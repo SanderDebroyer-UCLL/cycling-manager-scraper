19c5fadfb42e6b1d5e057fd72daebcc18fb8beba
SanderDebroyer-UCLL <sander.debroyer@student.ucll.be>
2025-05-22 15:27:47 +0200

    Added parcour type

---
+++ src/main/java/ucll/be/procyclingscraper/model/ParcoursType.java
@@ -0,0 +1,15 @@
+package ucll.be.procyclingscraper.model;
+
+public enum ParcoursType {
+    FLAT,
+    HILLY,
+    HILLY_HILL_FINISH,
+    MOUNTAIN,
+    MOUNTAIN_HILL_FINISH,
+
+    //Extra types
+    TIME_TRIAL,
+    INDIVIDUAL_TIME_TRIAL,
+    TEAM_TIME_TRIAL,
+    CLASSIC,
+}
