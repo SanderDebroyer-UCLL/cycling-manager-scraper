d77503fcbb1ebd7432adbf5213f3efddc575a191
BramGeboers <bramgeboers2@gmail.com>
2025-05-26 22:15:59 +0200

    Refactor RaceResultController and RaceResultService to return RaceResultWithCyclistDTO; add RaceResultWithCyclistDTO class

---
+++ src/main/java/ucll/be/procyclingscraper/dto/RaceResultWithCyclistDTO.java
@@ -0,0 +1,24 @@
+package ucll.be.procyclingscraper.dto;
+
+import java.time.LocalTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import ucll.be.procyclingscraper.model.RaceStatus;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+public class RaceResultWithCyclistDTO {
+    private Long id;
+    private String position;
+    private LocalTime time;
+    private RaceStatus raceStatus;
+    
+    private Long cyclistId;
+    private String cyclistName;
+    private String cyclistCountry;
+    // Add more fields as needed
+}
